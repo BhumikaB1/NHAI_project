a5beab9dec382486bbec29b09c742b6cfe9b8ffe
BhumikaB1 <bhumikapatrange077@gmail.com>
2026-07-23 12:52:30 +0530

    fixed gitignore to exclude build files

--- android/app/.cxx/Debug/1w5w34z5/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-nitro-image/react-native-nitro-imageConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-nitro-image::NitroImage)
-add_library(react-native-nitro-image::NitroImage SHARED IMPORTED)
-set_target_properties(react-native-nitro-image::NitroImage PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Bhumika/NHAI_project/node_modules/react-native-nitro-image/android/build/intermediates/cxx/Debug/c6upo1m6/obj/x86_64/libNitroImage.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Bhumika/NHAI_project/node_modules/react-native-nitro-image/android/build/headers/nitroimage"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/7017c2z4/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-nitro-modules/react-native-nitro-modulesConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-nitro-modules::NitroModules)
-add_library(react-native-nitro-modules::NitroModules SHARED IMPORTED)
-set_target_properties(react-native-nitro-modules::NitroModules PROPERTIES
-    IMPORTED_LOCATION "C:/Users/harsh/Desktop/Random/NHAI Project/node_modules/react-native-nitro-modules/android/build/intermediates/cxx/Debug/z601d44a/obj/arm64-v8a/libNitroModules.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/harsh/Desktop/Random/NHAI Project/node_modules/react-native-nitro-modules/android/build/headers/nitromodules"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
